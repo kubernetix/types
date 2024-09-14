5a86176310d2d0ab6a5c308633a80b6ae109c7df
nhh <niklas.hanft@gmail.com>
2024-09-14 13:56:05 +0200

    Bump version

--- index.d.ts
+++ index.d.ts
@@ -2,43 +2,6 @@ declare var React: any;
 
 declare namespace k8x {
   /**
-   *  The chart component can be used to configure a target cluster different that the default one. You can define multiple cluster
-   *
-   * @deprecated
-   * 
-   * @see {@link http://example.com/@internal | the @internal tag}
-   */
-  type Chart = {
-    /**
-     * This is the name of the chart
-     */
-    name: string;
-    /**
-     * This is the version of the chart
-     */
-    version: string;
-    /**
-     * This is the version of contained app
-     */
-    appVersion?: string;
-    /**
-     * This is the version of the k8s cluster
-     */
-    kubeVersion?: string;
-    description?: string;
-    type?: string;
-    keywords?: string[];
-    home?: URL;
-    sources?: string[];
-
-    maintainers?: string[];
-    icon?: URL;
-    deprecated?: boolean;
-    annotations?: string[];
-    //children: IntrinsicElements["k8s"];
-  };
-
-  /**
    *  The cluster component can be used to configure a target cluster different that the default one. You can define multiple cluster
    *
    * @example
@@ -99,10 +62,46 @@ declare namespace k8x {
 }
 
 declare namespace JSX {
-  interface ElementAttributesProperty {
-    props: {}; // specify the property name to use
-  }
   type IntrinsicElements = {
-    chart: k8x.Chart;
+    /**
+     *  The cluster component can be used to configure a target cluster different that the default one. You can define multiple cluster
+     *
+     * @example
+     * Possible children:
+     * ```tsx
+     * <Namespace />
+     *
+     * ```
+     * @see {@link http://example.com/@internal | the @internal tag}
+     */
+    chart: {
+      /**
+       * This is the name of the chart
+       */
+      name: string;
+      /**
+       * This is the version of the chart
+       */
+      version: string;
+      /**
+       * This is the version of contained app
+       */
+      appVersion?: string;
+      /**
+       * This is the version of the k8s cluster
+       */
+      kubeVersion?: string;
+      description?: string;
+      type?: string;
+      keywords?: string[];
+      home?: URL;
+      sources?: string[];
+
+      maintainers?: string[];
+      icon?: URL;
+      deprecated?: boolean;
+      annotations?: string[];
+      //children: IntrinsicElements["k8s"];
+    };
   };
 }

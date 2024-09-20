0adea54b7d445d53cd48c64677ebb93455e270c0
nhh <niklas.hanft@gmail.com>
2024-09-20 13:31:30 +0200

    Remove type generation
    Types are not usable for now

--- generate_types.sh
+++
@@ -1,4 +0,0 @@
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate --global-property models \
-    -i https://raw.githubusercontent.com/kubernetes/kubernetes/release-1.31/api/openapi-spec/swagger.json \
-    -g typescript \
-    -o /local/.gen
